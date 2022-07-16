06d617ea816cd2f0647b1314d50c52c97f39c5b5
sreelakshmi90 <pvsreelakshmi90@gmail.com>
2022-07-16 22:13:36 +0530

    2 files modified

--- src/User/Answers.js
+++ src/User/Answers.js
@@ -2,18 +2,24 @@ import React from 'react';
 
 
 export default function Answers() {
+  const [isAnswered,setIsAnswered] = useState(false);
   const answer = {
     type: 'checkbox',
-    values: ["computer Style Sheet", "Cascade Style Sheet" , "Creative Style Sheet"]
+    values: ["computer Style Sheet", "Cascade Style Sheet" , "Creative Style Sheet"],
+    answer: 2
   }
 
   const checkAnswer = () => {
+      isAnswered = true;
+      if(!isAnswered) {
 
+        setIsAnswered(true);
+      }
   }
  
   return (
     <div id="answers">
-      <ul>
+      <ul className="answer-list">
         { 
           answer.values.map((value,index) => (
             <li onClick={checkAnswer}
